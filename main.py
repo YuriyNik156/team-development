27d4289f1bcc0cccb3586c9e3ab82509eac45c86
YuriyNik156 <yuriy.nik156@gmail.com>
2025-07-07 16:34:47 +0300

    select info in data base

--- main.py
+++ main.py
@@ -2,17 +2,11 @@ import sqlite3
 
 conn = sqlite3.connect('example.db')
 cur = conn.cursor()
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS users
-    (id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INTEGER NOT NULL)
-        ''')
 
-conn.commit()
+cur.execute("SELECT * FROM users")
+rows = cur.fetchall()
 
-cur.execute("INSERT INTO users(name, age) VALUES (?, ?)", ('Алексей', 30))
-cur.execute("INSERT INTO users(name, age) VALUES (?, ?)", ('Игорь', 25))
+for row in rows:
+    print(row)
 
-conn.commit()
 conn.close()
